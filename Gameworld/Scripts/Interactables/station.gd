5664619be0af95683a7758d916e833e33a2f0fea
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-05-27 02:00:15 +0800

    Initial table code

--- Gameworld/Scripts/Interactables/station.gd
+++ Gameworld/Scripts/Interactables/station.gd
@@ -1,11 +1,12 @@
 class_name Station extends Interactable
 
+var item_on_table: Interactable
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func interact(player: Player):
+	if player.holding:
+		var held_item = player.holding
+		item_on_table = held_item
