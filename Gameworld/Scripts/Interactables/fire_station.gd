630f01a36db79f566c0047730a4b615b01173fe6
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-05-28 23:13:42 +0800

    Imported models for ingredients

--- Gameworld/Scripts/Interactables/fire_station.gd
+++ Gameworld/Scripts/Interactables/fire_station.gd
@@ -3,7 +3,7 @@ class_name FireStation extends Station
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	valid_cook_items = ["res://Gameworld/Scenes/barrel.tscn"] # TODO: Fill with cuttable ingredients
+	valid_cook_items = ["res://Gameworld/Scenes/barrel.tscn"] # TODO: Fill with cookable ingredients
 	max_items = 4
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
