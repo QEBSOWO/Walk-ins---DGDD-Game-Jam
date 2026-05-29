c7aa402c5db87578084c886b7d25a996f5981749
Joaquin Alonzo A. Salazar <geekythenerd@gmail.com>
2026-05-30 00:13:36 +0800

    Enemy spawning completed

--- Gameworld/Scripts/enemy_spawner.gd
+++ Gameworld/Scripts/enemy_spawner.gd
@@ -1,14 +1,36 @@
 extends Marker3D
 
+@onready var mob_timer: Timer = $MobTimer
+
 @export var enemy: PackedScene
-@export var respawn_duration: float = 1.0
-var active_enemy: Enemy
+@export var respawn_duration_min: float = 2
+@export var respawn_duration_max: float = 5
+
+@export var spawn_duration_min: float = 5
+@export var spawn_duration_max: float = 10
+
+@export var max_num_instances: int = 3
+
+var current_num_instances: int = 0
 
 func _ready() -> void:
 	enemy = load(enemy.get_path())
-	spawn_enemy()
+	mob_timer.timeout.connect(_on_mob_timer_timeout)
+
 
 func spawn_enemy() -> void:
-	active_enemy = enemy.instantiate()
+	var active_enemy: Enemy = enemy.instantiate()
 	add_child(active_enemy)
-	print("Spawned Enemy")
+	active_enemy.tree_exited.connect(_on_enemy_death)
+	current_num_instances += 1
+
+
+func _on_enemy_death() -> void:
+	current_num_instances -= 1
+
+
+func _on_mob_timer_timeout() -> void:
+	if current_num_instances < max_num_instances:
+		spawn_enemy()
+	mob_timer.wait_time = randf_range(spawn_duration_min, spawn_duration_max)
+	mob_timer.start()
