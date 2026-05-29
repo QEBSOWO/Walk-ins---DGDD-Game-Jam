f321a516b228412eb936ac4e7cf1253e006679f5
QEBSOWO <enrique.sinjian@student.ateneo.edu>
2026-05-29 23:58:08 +0800

    Refactored cutting minigame

--- Minigames/Scripts/cuttingbread.gd
+++ Minigames/Scripts/cuttingbread.gd
@@ -2,22 +2,34 @@ extends Node2D
 
 var sprite_arr = ["base", "cutlines", "cut_1", "cut_2"]
 var current_sprite = 0;
-var using_sword = true
+var using_sword = false
 var knife_positions = [0, 267, 381, 0]
 var knife_cuts = 0;
 var cutting = false;
+var damage = 1
 @onready var bread_sprite = $BreadSprite
 @onready var knife = $Knife
+@onready var sword = $Sword
 @onready var timer = $Timer
+
+var cutter : AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_change_current_sprite()
-	knife.play("default")
-	pass
+	enter(false)
 
-func enter():
-	pass
+func enter(has_sword):
+	self.using_sword = has_sword
+	if using_sword:
+		cutter = sword
+		knife.visible = false
+		damage = 10
+	else:
+		cutter = knife
+		sword.visible = false
+		damage = 1
+	cutter.play("default")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -26,24 +38,15 @@ func _process(delta):
 	_set_cutline_pos()
 
 func _set_cutline_pos():
-	knife.position = get_global_mouse_position()
+	cutter.position = get_global_mouse_position()
 
 func _cut_bread(sword : bool):
 	if Input.is_action_just_pressed("cut") and not cutting:
 		cutting = true;
 		timer.start()
-		knife.play("cutline")
+		cutter.play("cutline")
 		var mouse_x = get_global_mouse_position().x
-		print(get_global_mouse_position())
-		if (mouse_x < knife_positions[current_sprite] + 15
-		and mouse_x > knife_positions[current_sprite] - 15):
-			if(sword):
-				_change_current_sprite()
-			else:
-				knife_cuts += 1
-				if knife_cuts >= 10:
-					_change_current_sprite()
-					knife_cuts = 0
+		bread_sprite.change_current_sprite(damage)
 
 func _change_current_sprite():
 	current_sprite = current_sprite + 1
