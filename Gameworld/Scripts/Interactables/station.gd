4ed86d578f2c0cd6e76d0e150b8c79b0c38965fb
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-05-30 02:21:57 +0800

    Add mug interactable and include in stew recipe

--- Gameworld/Scripts/Interactables/station.gd
+++ Gameworld/Scripts/Interactables/station.gd
@@ -6,6 +6,8 @@ var can_cook: bool
 @export var recipes = Resource
 var cuttable = ["moss", "1l-oil", "Berries", "slime_ingredient", "bread"]
 var fireable = ["crab", "water", "water2", "pot", "spice"]
+var cutting = load("res://Minigames/Cutting/Scenes/cutting.tscn")
+var pouring = load("res://Minigames/Scenes/pouring.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -36,6 +38,9 @@ func interact(player_area: Area3D):
 			new_dish.global_position = self.global_position
 			new_dish.interact(player_area)
 			get_tree().root.get_child(0).add_child(new_dish)
+			
+			
+			
 			can_cook = false
 			for item in items_on_table:
 				item.queue_free()
