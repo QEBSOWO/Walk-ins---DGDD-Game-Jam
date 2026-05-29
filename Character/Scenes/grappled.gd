861ba41435810c735890a903fa16780330a0d862
Joaquin Alonzo A. Salazar <geekythenerd@gmail.com>
2026-05-30 01:59:36 +0800

    Encountered grappling bug if player exits at just the right time

--- Character/Scenes/grappled.gd
+++ Character/Scenes/grappled.gd
@@ -12,7 +12,7 @@ func enter(previous_state_path: String, data := {}) -> void:
 	player.velocity.x = 0.0
 	player.velocity.z = 0.0
 	player.take_damage(1)
-	
+
 
 func physics_update(_delta: float) -> void:
 	if !player.is_grappled:
