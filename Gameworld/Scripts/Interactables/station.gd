210d3bc3c529b62d54517ce8dcef40395c03060f
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-05-29 08:39:46 +0800

    Add character sprites and update to interactables

--- Gameworld/Scripts/Interactables/station.gd
+++ Gameworld/Scripts/Interactables/station.gd
@@ -1,31 +1,27 @@
 class_name Station extends Interactable
 
-@export var items_on_table: Array[Interactable]
-var valid_cook_items: Array[String] # Stores scene file paths for valid items 
+@export var items_on_table: Array[Ingredient]
+var can_cook: bool
 @export var max_items: int
+@export var recipes = Resource
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	items_on_table = []
-	valid_cook_items = []
+	can_cook = false
 
 func interact(player_area: Area3D):
 	var held_item = player_area.holding
 	if player_area.holding and items_on_table.size() < max_items:
 		items_on_table.push_front(held_item)
 		held_item.freeze_object()
-		held_item.global_position = self.global_position + Vector3(0, 0.5, 0)
-		print(str(items_on_table.size()) + "/" + str(max_items))
+		held_item.global_position = self.global_position + Vector3(0, 1, 0)
+		#print(str(items_on_table.size()) + "/" + str(max_items))
+		#if items_on_table.any(func(item): return item is Berries): print(items_on_table)
+		can_cook = recipes.check_recipe(items_on_table)
 	else:
-		var can_cook: bool = false
-		
-		for item in items_on_table:
-			if valid_cook_items.has(item.scene_file_path):
-				can_cook = true
-			else:
-				can_cook = false
-				break
-		
+		can_cook = recipes.check_recipe(items_on_table)
+		#print(can_cook)
 		if can_cook: print("Cooking minigame here")
 
 func release_item(player_area: Area3D):
